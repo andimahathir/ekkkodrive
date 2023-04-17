af95a6787c67bb94b2474d5c649bd23cca137727
andimahathir <enlandiy@ymail.com>
2023-04-17 08:47:27 +0700

    rapih2

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -14,6 +14,7 @@ export default function Login() {
     const [password, setPassword] = useState("");
     const [signInDisabled, setSignInDisabled] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("");
 
     useEffect(() => {
         GetCurrentUser();
@@ -32,13 +33,14 @@ export default function Login() {
 
     async function HandleSubmit(e) {
         e.preventDefault();
-        setSignInDisabled(true);
         try {
+            setSignInDisabled(true);
             const user = await Auth.signIn(username, password);
             console.log("user", user);
             push("/");
         } catch (error) {
-            console.log("error signing in", error);
+            setErrorMessage(error.message);
+            setSignInDisabled(false);
         }
     }
 
@@ -83,6 +85,32 @@ export default function Login() {
                             <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
                                 Sign in to your account
                             </h1>
+                            {errorMessage && (
+                                <div
+                                    class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+                                    role="alert"
+                                >
+                                    <svg
+                                        aria-hidden="true"
+                                        class="flex-shrink-0 inline w-5 h-5 mr-3"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                            clip-rule="evenodd"
+                                        ></path>
+                                    </svg>
+                                    <div>
+                                        <span class="font-medium">
+                                            {errorMessage}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+
                             <form
                                 className="space-y-4 md:space-y-6"
                                 onSubmit={HandleSubmit}
